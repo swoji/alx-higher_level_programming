7f3f73b38b38b7e07d3626ae70644855561fe9e7
swoji <davidookuku@gmail.com>
2023-01-03 21:19:42 +0300

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: 0 if there is no cycle, 1if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fire, *water;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	fire = list->next;
+	water = list->next->next;
+
+	while (fire && water && water->next)
+	{
+		if (fire == water)
+			return (1);
+
+		fire = fire->next;
+		water = water->next->next;
+	}
+
+	return (0);
+}
