f3270248f02e350fbfc101ce111d31d402ad7ae1
swoji <davidookuku@gmail.com>
2023-01-04 18:56:40 +0300

    prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number == 99:
-        print("{0:0=2d}".format(number))
+        print('{0:0=2d}'.format(number))
     else:
-        print("{0:0=2d}".format(number), end=', ')
+        print('{0:0=2d}'.format(number), end=', ')
